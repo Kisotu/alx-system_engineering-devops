3e503d2c91533b60143c71972088118afb09c194
Oloishorua <jacoboloishorua@gmail.com>
2024-06-04 22:02:42 +0300

    how many subs  - correction header case fix

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -9,7 +9,7 @@ def number_of_subscribers(subreddit):
 
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
 
-    headers = {'user-agent': 'my-app/0.0.1'}
+    headers = {'User-Agent': 'my-app/0.0.1'}
 
     req = get(url, headers=headers, allow_redirects=False)
 
